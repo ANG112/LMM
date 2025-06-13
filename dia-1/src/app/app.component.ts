4311396a9712f55e7c61db7375d6e6d05b38def2
Alfonso Nieto García <43706214+ANG112@users.noreply.github.com>
2025-06-13 14:20:18 +0200

    Encabezado y usuario

--- dia-1/src/app/app.component.ts
+++ dia-1/src/app/app.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+
+import { EncabezadoComponent } from "./encabezado/encabezado.component";
+import { UsuarioComponent } from "./usuario/usuario.component";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [EncabezadoComponent, UsuarioComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
---
+++ dia-1/src/app/usuario/usuario.component.ts
@@ -0,0 +1,25 @@
+import { Component } from '@angular/core';
+import { USUARIOS_FALSOS } from './usuarios-falsos';
+// Selecciona (supuestamente) una foto de la 
+const indiceAleatorio = Math.floor(Math.random() * USUARIOS_FALSOS.length)
+
+@Component({
+  selector: 'app-usuario',
+  standalone: true,
+  imports: [],
+  templateUrl: './usuario.component.html',
+  styleUrl: './usuario.component.css'
+})
+
+export class UsuarioComponent {
+  usuarioSeleccionado = USUARIOS_FALSOS[indiceAleatorio]
+
+  get rutaImagen () {
+    return '../../../public/usuarios' + this.usuarioSeleccionado.avatar
+  }
+
+  alSeleccionarUsuario () {
+    const indiceAleatorio = Math.floor(Math.random() * USUARIOS_FALSOS.length)
+    this.usuarioSeleccionado = USUARIOS_FALSOS[indiceAleatorio]
+  }
+}
