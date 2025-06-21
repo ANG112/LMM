f56717495b98cd34a97cbceb596776053fbaf7ec
Alfonso Nieto García <43706214+ANG112@users.noreply.github.com>
2025-06-21 14:51:20 +0200

    revisar la configuración

--- dia-5/Proyecto_prueba/src/app/views/pagina-independiente/pagina-independiente.component.ts
+++ dia-5/Proyecto_prueba/src/app/views/pagina-independiente/pagina-independiente.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
 import { HeaderComponent } from '../../object/header/header.component';
+import { NavComponent } from '../../object/nav/nav.component';
+import { MainComponent } from '../../object/main/main.component';
 
 @Component({
   selector: 'app-pagina-independiente',
-  imports: [HeaderComponent],
+  imports: [HeaderComponent, NavComponent, MainComponent],
   templateUrl: './pagina-independiente.component.html',
   styleUrl: './pagina-independiente.component.css'
 })
